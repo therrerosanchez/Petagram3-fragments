037ec3a0212dcd7478c8b2f01804d2a5aa004609
therrerosanchez <32817685+therrerosanchez@users.noreply.github.com>
2018-05-14 06:33:19 +0200

    ok

---
+++ src/main/java/org/therrero/petagram3/Config.java
@@ -0,0 +1,6 @@
+package org.therrero.petagram3;
+
+public class Config {
+    public static final String EMAIL ="practicastherrero@gmail.com";
+    public static final String PASSWORD ="practicasandroid";
+}
